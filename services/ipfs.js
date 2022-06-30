e03fb2e8202be9a6f898a1d0867f22fe40d1bc11
Siarhei Mamykin <bymamykin@gmail.com>
2022-06-30 19:10:50 +0400

    added services

---
+++ services/ipfs.js
@@ -0,0 +1,24 @@
+import { create } from 'ipfs-http-client';
+import fs from 'fs';
+
+const client = create();
+
+export const imageUploadIpfs = async (fileToUpload) => {
+  const file = fs.readFileSync(fileToUpload.path);
+  const filesAdded = await client.add(
+    { path: fileToUpload.filename, content: file },
+    {
+      progress: (len) => console.log('Uploading file...' + len),
+    }
+  );
+  const fileHash = filesAdded.cid.toString();
+  console.log(fileHash);
+  return fileHash;
+};
+
+export const metadataUploadIpfs = async (file) => {
+  const metadataAdded = await client.add(file);
+  const metadataHash = metadataAdded.cid.toString();
+  console.log(metadataHash);
+  return metadataHash;
+};
--- services/upload.js
+++ services/upload.js
@@ -1,24 +1,43 @@
 import multer from 'multer';
-import path from "path"
+import path from 'path';
 
 const videoStorage = multer.diskStorage({
-    destination: 'videos', // Destination to store video 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '_' + Date.now() 
-         + path.extname(file.originalname))
-    }
+  destination: 'public', // Destination to store video
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + '_' + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 export const videoUpload = multer({
-    storage: videoStorage,
-    limits: {
-    fileSize: 10000000 // 10000000 Bytes = 10 MB
-    },
-    fileFilter(req, file, cb) {
-      // upload only mp4 and mkv format
-      if (!file.originalname.match(/\.(mp4|MPEG-4|mkv)$/)) { 
-         return cb(new Error('Please upload a video'))
-      }
-      cb(undefined, true)
-   }
-})
+  storage: videoStorage,
+  limits: {
+    fileSize: 10000000, // 10000000 Bytes = 10 MB
+  },
+  fileFilter(req, file, cb) {
+    // upload only mp4 and mkv format
+    if (!file.originalname.match(/\.(mp4|MPEG-4|mkv)$/)) {
+      return cb(new Error('Please upload a video'));
+    }
+    cb(undefined, true);
+  },
+});
+
+const imageStorage = multer.diskStorage({
+  destination: 'public', // Destination to store video
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + '_' + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+export const imageUpload = multer({
+  storage: imageStorage,
+  limits: {
+    fileSize: 10000000, // 10000000 Bytes = 10 MB
+  },
+});
